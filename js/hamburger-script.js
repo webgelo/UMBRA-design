57813bb4373666fbf2801fabaecb5daa0887e854
webgelo <moiseevou@mail.ru>
2024-01-29 17:00:39 +0300

    29.01.2024 3

---
+++ js/hamburger-script.js
@@ -0,0 +1,17 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.nav-menu__list'),
+    menuItem = document.querySelectorAll('.nav-menu__item'),
+    hamburger = document.querySelector('.hamburger');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('nav-menu__list_active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('nav-menu__list_active');
+        })
+    })
+})
